c2551e6e9cb9150e4ccc7722b3c9ff275165cf9f
stephenmayowa112 <stephenmayowa112@gmail.com>
2025-03-09 18:41:07 +0100

    Feat: wrap LoginPage in Suspense for improved loading state handling

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react';
 import LoginPage from './LoginPage';
 
-export default function Page() {
-  return <LoginPage />;
+export default function Page(): React.JSX.Element {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginPage />
+    </Suspense>
+  );
 }
