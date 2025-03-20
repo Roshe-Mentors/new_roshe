d0e3ab131fdd460ded433e2d938d8e3eec56de51
stephenmayowa112 <stephenmayowa112@gmail.com>
2025-03-20 06:56:09 +0100

    Feat: update global font to Poppins and adjust layout for improved typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,15 @@
 import React from 'react'
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Montserrat } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-const montserrat = Montserrat({
+const poppins = Poppins({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   subsets: ['latin'],
-  variable: '--font-montserrat',
-});
-
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-poppins',
+  display: 'swap'
 });
 
 export const metadata: Metadata = {
@@ -50,13 +42,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${montserrat.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" className={`${poppins.variable}`}>
+      <body className={`${poppins.className}`}>
         <Navbar />
-        <Footer />
+        {children}
+        <Footer />  
       </body>
     </html>
   );
