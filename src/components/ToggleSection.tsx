a1517fa6ac621153796f03f16c9a47817928410e
stephenmayowa112 <stephenmayowa112@gmail.com>
2025-03-27 05:42:44 +0100

    FEAT: adjust ToggleSection dropdown dimensions and styling

--- src/components/ToggleSection.tsx
+++ src/components/ToggleSection.tsx
@@ -196,8 +196,8 @@ const ToggleSection: React.FC = () => {
                   
                   {isOpen && (
                     <div
-                      className="absolute z-50 bg-white border border-gray-300 rounded-md shadow-lg mt-1 overflow-y-auto max-h-[400px] md:max-h-[50vh] w-full"
-                      style={{ maxWidth: '500px' }}
+                      className="absolute z-50 bg-white border border-gray-300 rounded-md shadow-lg mt-1 overflow-y-auto max-h-[300px] md:max-h-[40vh]"
+                      style={{ width: 'calc(4/6*100%)', maxWidth: '400px' , left: '0'}}
                       onMouseLeave={handleMenuLeave}
                     >
                       {/* Menu items in a single column */}
