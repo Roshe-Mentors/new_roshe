117ecfde73751e29c2e54d2f7e0f2160556d2e47
stephenmayowa112 <stephenmayowa112@gmail.com>
2025-03-31 09:42:55 +0100

    FEAT: enhance ToggleSection layout with improved flexbox styling for better alignment

--- src/components/ToggleSection.tsx
+++ src/components/ToggleSection.tsx
@@ -290,7 +290,7 @@ const ToggleSection: React.FC = () => {
                 <p className="text-gray-600 mb-6 md:mb-8 text-lg md:text-xl leading-relaxed px-8 md:px-0">
                   Fast-track your career with personalized 1:1 guidance from over 1000 expert mentors in our community.
                 </p>
-                <div className="relative px-4 md:px-0" ref={dropdownRef}>
+                <div className="relative px-4 md:px-0 flex justify-center" ref={dropdownRef}>
                   <button
                     ref={buttonRef}
                     onClick={() => setIsOpen(!isOpen)}
@@ -304,7 +304,9 @@ const ToggleSection: React.FC = () => {
                       >
                         <path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path>
                       </svg>
-                      <span>{selectedSkill || "What skill do you want to improve?"}</span>
+                      <span className="flex items-center justify-center w-full">
+                        {selectedSkill || "What skill do you want to improve?"}
+                      </span>
                     </div>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
